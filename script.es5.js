1b7384bc609061ce09dca947fa315b22c69a12df
Alex White <aaapwww@gmail.com>
2017-04-03 00:44:52 +0000

    Added webpage files

---
+++ script.es5.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var accessToken;
+var canvas = void 0,
+    c = void 0;
+var runOnce = false;
+var sideLength = void 0;
+var size = void 0;
+var cols = void 0,
+    rows = void 0;
+var downloaded = void 0;
+
+function getSearchParameters() {
+	var prmstr = window.location.search.substr(1);
+	return prmstr != null && prmstr != '' ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray(prmstr) {
+	var params = {};
+	var prmarr = prmstr.split('&');
+	for (var i = 0; i < prmarr.length; i++) {
+		var tmparr = prmarr[i].split('=');
+		params[tmparr[0]] = tmparr[1];
+	}
+	return params;
+}
+
+$(function () {
+	$('#copyright').css('display', 'block').html('Copyright &copy; Alex White  ' + new Date().getFullYear());
+	$('#form').submit(function (e) {
+		e.preventDefault();
+		getAlbums();
+	});
+	canvas = document.getElementById('canvas');
+	c = canvas.getContext('2d');
+});
+
+function getAlbums() {
+	$('#canvasImg').remove();
+	downloaded = 0;
+	$('#canvas').css('display', 'inline');
+	$('#loading').css('display', 'block');
+	var API_KEY = 'b7cad0612089bbbfecfc08acc52087f1';
+	var username = $('#username').val();
+	var period = $('#duration').find(':selected').val();
+	rows = parseInt($('#rows').find(':selected').val());
+	cols = parseInt($('#columns').find(':selected').val());
+	var limit = rows * cols;
+	size = parseInt($('#size').find(':selected').val());
+	switch (size) {
+		case 0:
+			sideLength = 34;
+			break;
+		case 1:
+			sideLength = 64;
+			break;
+		case 2:
+			sideLength = 174;
+			break;
+		default:
+			sideLength = 300;
+			break;
+	}
+	var URL = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' + username + '&period=' + period + '&api_key=' + API_KEY + '&limit=' + limit + '&format=json';
+	$.ajax(URL, {
+		type: 'GET',
+		dataType: 'json',
+		success: function success(data, status, xhr) {
+			canvas.width = sideLength * cols;
+			canvas.height = sideLength * rows;
+
+			c.fillStyle = 'black';
+			c.fillRect(0, 0, canvas.width, canvas.height);
+
+			var links = data.topalbums.album.map(function (album) {
+				return album.image[size]['#text'];
+			});
+			var maxX = sideLength * (cols - 1);
+			var maxY = sideLength * (rows - 1);
+			for (var i = 0, k = 0; i < rows; i++) {
+				for (var j = 0; j < cols; j++, k++) {
+					printAlbum(links[k], j, i);
+				}
+			}
+		},
+		error: function error(xhr, status, _error) {
+			console.log(status, _error);
+			alert('There was an error');
+		}
+	});
+}
+
+function printAlbum(link, i, j) {
+	var img = new Image(sideLength, sideLength);
+	img.crossOrigin = 'Anonymous';
+	img.onload = function () {
+		c.drawImage(img, i * sideLength, j * sideLength);
+		downloaded++;
+		if (downloaded === cols * rows) {
+			$('#loading').css('display', 'none');
+			$('#canvas').css('display', 'none');
+			var canvasImg = new Image(sideLength * cols, sideLength * rows);
+			canvasImg.style = 'margin:10px;';
+			canvasImg.crossOrigin = 'Anonymous';
+			canvasImg.src = canvas.toDataURL('image/png');
+			canvasImg.id = 'canvasImg';
+			$('#generated').append(canvasImg);
+		}
+	};
+	img.src = link;
+}
---
+++ script.js
@@ -0,0 +1,100 @@
+var accessToken;
+let canvas, c;
+let runOnce = false;
+let sideLength;
+let size;
+let cols, rows;
+let downloaded = 0;
+
+function getSearchParameters() {
+	var prmstr = window.location.search.substr(1);
+	return prmstr != null && prmstr != '' ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray(prmstr) {
+	var params = {};
+	var prmarr = prmstr.split('&');
+	for (var i = 0; i < prmarr.length; i++) {
+		var tmparr = prmarr[i].split('=');
+		params[tmparr[0]] = tmparr[1];
+	}
+	return params;
+}
+
+$(function() {
+	$('#copyright').css('display', 'block').html('Copyright &copy; Alex White  ' + new Date().getFullYear());
+	$('#form').submit((e) => {
+		e.preventDefault();
+		getAlbums();
+	});
+	canvas = document.getElementById('canvas');
+	c = canvas.getContext('2d');
+});
+
+function getAlbums() {
+	$('#loading').css('display', 'block');
+	const API_KEY = 'b7cad0612089bbbfecfc08acc52087f1';
+	let username = $('#username').val();
+	let period = $('#duration').find(':selected').val();
+	rows = parseInt($('#rows').find(':selected').val());
+	cols = parseInt($('#columns').find(':selected').val());
+	let limit = rows * cols;
+	size = parseInt($('#size').find(':selected').val());
+	switch (size) {
+		case 0:
+		sideLength = 34;
+		break;
+		case 1:
+		sideLength = 64;
+		break;
+		case 2:
+		sideLength = 174;
+		break;
+		default:
+		sideLength = 300;
+		break;
+	}
+	const URL = `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${username}&period=${period}&api_key=${API_KEY}&limit=${limit}&format=json`;
+	$.ajax(URL, {
+		type: 'GET',
+		dataType: 'json',
+		success: function(data, status, xhr) {
+			canvas.width = sideLength * cols;
+			canvas.height = sideLength * rows;
+
+			c.fillStyle = 'black';
+			c.fillRect(0, 0, canvas.width, canvas.height);
+
+			let links = data.topalbums.album.map((album) => album.image[size]['#text']);
+			let maxX = sideLength * (cols - 1);
+			let maxY = sideLength * (rows - 1);
+			for (let i = 0, k = 0; i < rows; i++) {
+				for (let j = 0; j < cols; j++, k++) {
+					printAlbum(links[k], j, i);
+				}
+			}
+		},
+		error: function(xhr, status, error) {
+			console.log(status, error);
+			alert('There was an error');
+		}
+	});
+}
+
+function printAlbum(link, i, j) {
+	let img = new Image(sideLength, sideLength);
+	img.onload = function() {
+		c.drawImage(img, i * sideLength, j * sideLength);
+		downloaded++;
+		if (downloaded === cols * rows) {
+			$('#loading').css('display', 'none');
+			$('#canvas').css('display', 'none');
+			let canvasImg = new Image(sideLength * cols, sideLength * rows);
+			canvasImg.src = canvas.toDataURL('image/png');
+			canvasImg.crossOrigin = 'Anonymous';
+			$('#generated').append(canvasImg);
+		}
+	};
+	img.src = link;
+}
+
