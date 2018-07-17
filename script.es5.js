dc95bb372964840f2577368a4f336330a08bb933
Alex White <a24white@uwaterloo.ca>
2018-07-17 18:05:19 -0400

    Refactoring

--- script.es5.js
+++ script.es5.js
@@ -2,10 +2,8 @@
 
 var canvas = void 0,
     c = void 0;
-var sideLength = void 0;
-var cols = void 0,
-    rows = void 0;
 var downloaded = 0;
+var collageInfo = {};
 var METHOD_ALBUMS = 1;
 var METHOD_ARTISTS = 2;
 
@@ -15,8 +13,12 @@ $(function () {
 		e.preventDefault();
 		localStorage.username = $('#username').val().trim();
 		localStorage.period = $('#period').find(':selected').val();
-		localStorage.rows = $('#rows').find(':selected').val(), localStorage.cols = $('#cols').find(':selected').val(), localStorage.size = $('#size').find(':selected').val(), localStorage.method = $('#method').find(':selected').val(), localStorage.showName = $('#showName').is(':checked');
-		getContent();
+		localStorage.rows = $('#rows').find(':selected').val();
+		localStorage.cols = $('#cols').find(':selected').val();
+		localStorage.size = $('#size').find(':selected').val();
+		localStorage.method = $('#method').find(':selected').val();
+		localStorage.showName = $('#showName').is(':checked');
+		submit();
 	});
 	$('#method').change(function (e) {
 		setOverlayLabel();
@@ -51,82 +53,74 @@ function setOverlayLabel() {
 	}
 }
 
-function getContent() {
-	rows = parseInt(localStorage.rows);
-	cols = parseInt(localStorage.cols);
+function submit() {
+	downloaded = 0;
+
+	setCollageInfo();
+	initCanvas();
+
+	// Last.fm specific
 	var username = localStorage.username;
 	var period = localStorage.period;
-	var size = parseInt(localStorage.size);
-	var method = parseInt(localStorage.method);
-	var showName = localStorage.showName === 'true';
-	$('#canvasImg').remove();
-	downloaded = 0;
-	$('#canvas').css('display', 'inline');
-	$('#loading').css('display', 'block');
 	var API_KEY = 'b7cad0612089bbbfecfc08acc52087f1';
-	var limit = rows * cols;
-	switch (size) {
-		case 0:
-			sideLength = 34;
-			break;
-		case 1:
-			sideLength = 64;
-			break;
-		case 2:
-			sideLength = 174;
-			break;
-		default:
-			sideLength = 300;
-			break;
-	}
-	var url = void 0;
-	switch (method) {
+	var limit = collageInfo.rows * collageInfo.cols;
+
+	switch (collageInfo.method) {
 		case METHOD_ALBUMS:
-			url = '//ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' + username + '&period=' + period + '&api_key=' + API_KEY + '&limit=' + limit + '&format=json';
+			collageInfo.url = '//ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' + username + '&period=' + period + '&api_key=' + API_KEY + '&limit=' + limit + '&format=json';
 			break;
 		case METHOD_ARTISTS:
-			url = '//ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=' + username + '&period=' + period + '&api_key=' + API_KEY + '&limit=' + limit + '&format=json';
+			collageInfo.url = '//ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=' + username + '&period=' + period + '&api_key=' + API_KEY + '&limit=' + limit + '&format=json';
 			break;
 	}
-	$.ajax(url, {
+	// End Last.fm specific
+
+	getImageLinks();
+}
+
+function setCollageInfo() {
+	collageInfo.showName = localStorage.showName === 'true';
+	collageInfo.method = parseInt(localStorage.method);
+	collageInfo.size = parseInt(localStorage.size);
+	collageInfo.rows = parseInt(localStorage.rows);
+	collageInfo.cols = parseInt(localStorage.cols);
+
+	collageInfo.imageNum = collageInfo.rows * collageInfo.cols;
+
+	switch (collageInfo.size) {
+		case 0:
+			collageInfo.sideLength = 34;
+			break;
+		case 1:
+			collageInfo.sideLength = 64;
+			break;
+		case 2:
+			collageInfo.sideLength = 174;
+			break;
+		default:
+			collageInfo.sideLength = 300;
+			break;
+	}
+}
+
+function initCanvas() {
+	$('#canvasImg').remove();
+	$('#canvas').css('display', 'inline');
+	$('#loading').css('display', 'block');
+
+	canvas.width = collageInfo.sideLength * collageInfo.cols;
+	canvas.height = collageInfo.sideLength * collageInfo.rows;
+
+	c.fillStyle = 'black';
+	c.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function getImageLinks() {
+	$.ajax(collageInfo.url, {
 		type: 'GET',
 		dataType: 'json',
 		success: function success(data, status, xhr) {
-			canvas.width = sideLength * cols;
-			canvas.height = sideLength * rows;
-
-			c.fillStyle = 'black';
-			c.fillRect(0, 0, canvas.width, canvas.height);
-
-			var links = void 0,
-			    titles = void 0;
-			switch (method) {
-				case METHOD_ALBUMS:
-					links = data.topalbums.album.map(function (album) {
-						return album.image[size]['#text'];
-					});
-					titles = data.topalbums.album.map(function (album) {
-						return album.artist.name + ' – ' + album.name;
-					});
-					break;
-				case METHOD_ARTISTS:
-					links = data.topartists.artist.map(function (artist) {
-						return artist.image[size]['#text'];
-					});
-					titles = data.topartists.artist.map(function (artist) {
-						return artist.name;
-					});
-					break;
-			}
-			for (var i = 0, k = 0; i < rows; i++) {
-				for (var j = 0; j < cols; j++, k++) {
-					if (!links[k] || links[k].length === 0) {
-						printItem(null, j, i, titles[k], true);
-					} else {
-						printItem(links[k], j, i, titles[k], showName);
-					}
-				}
-			}
+			makeCollage(data);
 		},
 		error: function error(xhr, status, _error) {
 			console.log(status, _error);
@@ -135,16 +129,50 @@ function getContent() {
 	});
 }
 
-function printItem(link, i, j, title, showName) {
+function makeCollage(data) {
+	var links = void 0,
+	    titles = void 0;
+
+	switch (collageInfo.method) {
+		case METHOD_ALBUMS:
+			links = data.topalbums.album.map(function (album) {
+				return album.image[collageInfo.size]['#text'];
+			});
+			titles = data.topalbums.album.map(function (album) {
+				return album.artist.name + ' – ' + album.name;
+			});
+			break;
+		case METHOD_ARTISTS:
+			links = data.topartists.artist.map(function (artist) {
+				return artist.image[collageInfo.size]['#text'];
+			});
+			titles = data.topartists.artist.map(function (artist) {
+				return artist.name;
+			});
+			break;
+	}
+
+	for (var i = 0, k = 0; i < collageInfo.rows; i++) {
+		for (var j = 0; j < collageInfo.cols; j++, k++) {
+			if (!links[k] || links[k].length === 0) {
+				loadImage(null, j, i, titles[k], true);
+			} else {
+				loadImage(links[k], j, i, titles[k], collageInfo.showName);
+			}
+		}
+	}
+}
+
+function loadImage(link, i, j, title, showName) {
 	if (!link) {
 		printName(i, j, title);
 		registerDownloaded();
 	} else {
-		var img = new Image(sideLength, sideLength);
+		var img = new Image(collageInfo.sideLength, collageInfo.sideLength);
 		img.crossOrigin = 'Anonymous';
 		img.classList.add('img-responsive');
 		img.onload = function () {
-			c.drawImage(img, i * sideLength, j * sideLength);
+			c.drawImage(img, i * collageInfo.sideLength, j * collageInfo.sideLength);
 			if (showName) {
 				printName(i, j, title, true);
 			}
@@ -158,10 +186,10 @@ function printName(i, j, title) {
 	var overlay = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
 
 	c.textAlign = 'center';
-	var fontSize = Math.min(sideLength * 1.3 / title.length, sideLength / 15);
+	var fontSize = Math.min(collageInfo.sideLength * 1.3 / title.length, collageInfo.sideLength / 15);
 	c.font = fontSize + 'pt sans-serif';
 	c.fillStyle = 'white';
-	var textX = i * sideLength + sideLength / 2;
+	var textX = i * collageInfo.sideLength + collageInfo.sideLength / 2;
 	var textY = void 0;
 	if (overlay) {
 		c.shadowBlur = 5;
@@ -169,9 +197,9 @@ function printName(i, j, title) {
 		c.shadowOffsetX = 2;
 		c.shadowOffsetY = 2;
 		c.textBaseline = 'bottom';
-		textY = j * sideLength + sideLength - sideLength / 30;
+		textY = j * collageInfo.sideLength + collageInfo.sideLength - collageInfo.sideLength / 30;
 	} else {
-		textY = j * sideLength + sideLength / 2;
+		textY = j * collageInfo.sideLength + collageInfo.sideLength / 2;
 		c.textBaseline = 'middle';
 	}
 	c.fillText(title, textX, textY);
@@ -179,10 +207,10 @@ function printName(i, j, title) {
 
 function registerDownloaded() {
 	downloaded++;
-	if (downloaded === cols * rows) {
+	if (downloaded === collageInfo.imageNum) {
 		$('#loading').css('display', 'none');
 		$('#canvas').css('display', 'none');
-		var canvasImg = new Image(sideLength * cols, sideLength * rows);
+		var canvasImg = new Image(collageInfo.sideLength * collageInfo.cols, collageInfo.sideLength * collageInfo.rows);
 		canvasImg.src = canvas.toDataURL('image/png');
 		canvasImg.classList.add('img-responsive');
 		canvasImg.crossOrigin = 'Anonymous';
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let canvas, c;
-let sideLength;
-let cols, rows;
 let downloaded = 0;
+const collageInfo = {};
 const METHOD_ALBUMS = 1;
 const METHOD_ARTISTS = 2;
 
@@ -16,7 +15,7 @@ $(function() {
 		localStorage.size = $('#size').find(':selected').val();
 		localStorage.method = $('#method').find(':selected').val();
 		localStorage.showName = $('#showName').is(':checked');
-		getContent();
+		submit();
 	});
 	$('#method').change((e) => {
 		setOverlayLabel();
@@ -51,73 +50,74 @@ function setOverlayLabel() {
 	}
 }
 
-function getContent() {
-	rows = parseInt(localStorage.rows);
-	cols = parseInt(localStorage.cols);
+function submit() {
+	downloaded = 0;
+
+	setCollageInfo();
+	initCanvas();
+	
+	// Last.fm specific
 	const username = localStorage.username;
 	const period = localStorage.period;
-	const size = parseInt(localStorage.size);
-	const method = parseInt(localStorage.method);
-	const showName = localStorage.showName === 'true';
-	$('#canvasImg').remove();
-	downloaded = 0;
-	$('#canvas').css('display', 'inline');
-	$('#loading').css('display', 'block');
 	const API_KEY = 'b7cad0612089bbbfecfc08acc52087f1';
-	let limit = rows * cols;
-	switch (size) {
-		case 0:
-			sideLength = 34;
-			break;
-		case 1:
-			sideLength = 64;
-			break;
-		case 2:
-			sideLength = 174;
-			break;
-		default:
-			sideLength = 300;
-			break;
-	}
-	let url;
-	switch(method) {
+	const limit = collageInfo.rows * collageInfo.cols;
+	
+	switch(collageInfo.method) {
 		case METHOD_ALBUMS:
-			url = `//ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${username}&period=${period}&api_key=${API_KEY}&limit=${limit}&format=json`;
+			collageInfo.url = `//ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${username}&period=${period}&api_key=${API_KEY}&limit=${limit}&format=json`;
 			break;
 		case METHOD_ARTISTS:
-			url = `//ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${username}&period=${period}&api_key=${API_KEY}&limit=${limit}&format=json`;
+			collageInfo.url = `//ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${username}&period=${period}&api_key=${API_KEY}&limit=${limit}&format=json`;
 			break;
 	}
-	$.ajax(url, {
+	// End Last.fm specific
+	
+	getImageLinks();
+}
+
+function setCollageInfo() {
+	collageInfo.showName = localStorage.showName === 'true';
+	collageInfo.method = parseInt(localStorage.method);
+	collageInfo.size = parseInt(localStorage.size);
+	collageInfo.rows = parseInt(localStorage.rows);
+	collageInfo.cols = parseInt(localStorage.cols);
+
+	collageInfo.imageNum = collageInfo.rows * collageInfo.cols;
+	
+	switch (collageInfo.size) {
+		case 0:
+			collageInfo.sideLength = 34;
+			break;
+		case 1:
+			collageInfo.sideLength = 64;
+			break;
+		case 2:
+			collageInfo.sideLength = 174;
+			break;
+		default:
+			collageInfo.sideLength = 300;
+			break;
+	}
+}
+
+function initCanvas() {
+	$('#canvasImg').remove();
+	$('#canvas').css('display', 'inline');
+	$('#loading').css('display', 'block');
+
+	canvas.width = collageInfo.sideLength * collageInfo.cols;
+	canvas.height = collageInfo.sideLength * collageInfo.rows;
+
+	c.fillStyle = 'black';
+	c.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function getImageLinks() {
+	$.ajax(collageInfo.url, {
 		type: 'GET',
 		dataType: 'json',
 		success: function(data, status, xhr) {
-			canvas.width = sideLength * cols;
-			canvas.height = sideLength * rows;
-
-			c.fillStyle = 'black';
-			c.fillRect(0, 0, canvas.width, canvas.height);
-
-			let links, titles;
-			switch(method) {
-				case METHOD_ALBUMS:
-					links = data.topalbums.album.map((album) => album.image[size]['#text']);
-					titles = data.topalbums.album.map((album) => album.artist.name + ' – ' + album.name);
-					break;
-				case METHOD_ARTISTS:
-					links = data.topartists.artist.map((artist) => artist.image[size]['#text']);
-					titles = data.topartists.artist.map((artist) => artist.name);
-					break;
-			}
-			for (let i = 0, k = 0; i < rows; i++) {
-				for (let j = 0; j < cols; j++, k++) {
-					if (!links[k] || links[k].length === 0) {
-						printItem(null, j, i, titles[k], true);
-					} else {
-						printItem(links[k], j, i, titles[k], showName);
-					}
-				}
-			}
+			makeCollage(data);
 		},
 		error: function(xhr, status, error) {
 			console.log(status, error);
@@ -126,16 +126,41 @@ function getContent() {
 	});
 }
 
-function printItem(link, i, j, title, showName) {
+function makeCollage(data) {
+	let links, titles;
+
+	switch (collageInfo.method) {
+		case METHOD_ALBUMS:
+			links = data.topalbums.album.map((album) => album.image[collageInfo.size]['#text']);
+			titles = data.topalbums.album.map((album) => album.artist.name + ' – ' + album.name);
+			break;
+		case METHOD_ARTISTS:
+			links = data.topartists.artist.map((artist) => artist.image[collageInfo.size]['#text']);
+			titles = data.topartists.artist.map((artist) => artist.name);
+			break;
+	}
+
+	for (let i = 0, k = 0; i < collageInfo.rows; i++) {
+		for (let j = 0; j < collageInfo.cols; j++, k++) {
+			if (!links[k] || links[k].length === 0) {
+				loadImage(null, j, i, titles[k], true);
+			} else {
+				loadImage(links[k], j, i, titles[k], collageInfo.showName);
+			}
+		}
+	}
+}
+
+function loadImage(link, i, j, title, showName) {
 	if (!link) {
 		printName(i, j, title);
 		registerDownloaded();
 	} else {
-		let img = new Image(sideLength, sideLength);
+		let img = new Image(collageInfo.sideLength, collageInfo.sideLength);
 		img.crossOrigin = 'Anonymous';
 		img.classList.add('img-responsive');
 		img.onload = function() {
-			c.drawImage(img, i * sideLength, j * sideLength);
+			c.drawImage(img, i * collageInfo.sideLength, j * collageInfo.sideLength);
 			if (showName) {
 				printName(i, j, title, true);
 			}
@@ -147,10 +172,10 @@ function printItem(link, i, j, title, showName) {
 
 function printName(i, j, title, overlay = false) {
 	c.textAlign = 'center';
-	const fontSize = Math.min(sideLength * 1.3 / title.length, sideLength / 15);
+	const fontSize = Math.min(collageInfo.sideLength * 1.3 / title.length, collageInfo.sideLength / 15);
 	c.font = `${fontSize}pt sans-serif`;
 	c.fillStyle = 'white';
-	const textX = i * sideLength + sideLength / 2;
+	const textX = i * collageInfo.sideLength + collageInfo.sideLength / 2;
 	let textY;
 	if (overlay) {
 		c.shadowBlur = 5;
@@ -158,9 +183,9 @@ function printName(i, j, title, overlay = false) {
 		c.shadowOffsetX = 2;
 		c.shadowOffsetY = 2;
 		c.textBaseline = 'bottom';
-		textY = j * sideLength + sideLength - sideLength / 30;
+		textY = j * collageInfo.sideLength + collageInfo.sideLength - collageInfo.sideLength / 30;
 	} else {
-		textY = j * sideLength + sideLength / 2;
+		textY = j * collageInfo.sideLength + collageInfo.sideLength / 2;
 		c.textBaseline = 'middle';
 	}
 	c.fillText(title, textX, textY);
@@ -168,10 +193,10 @@ function printName(i, j, title, overlay = false) {
 
 function registerDownloaded() {
 	downloaded++;
-	if (downloaded === cols * rows) {
+	if (downloaded === collageInfo.imageNum) {
 		$('#loading').css('display', 'none');
 		$('#canvas').css('display', 'none');
-		let canvasImg = new Image(sideLength * cols, sideLength * rows);
+		let canvasImg = new Image(collageInfo.sideLength * collageInfo.cols, collageInfo.sideLength * collageInfo.rows);
 		canvasImg.src = canvas.toDataURL('image/png');
 		canvasImg.classList.add('img-responsive');
 		canvasImg.crossOrigin = 'Anonymous';
